40da5c45a1ece35264fe9b6819da55ebf6dd796d
manish <manish.sharma@antechindia.com>
2023-07-07 09:47:29 +0530

    pushing the code

---
+++ README.md
@@ -0,0 +1,16 @@
+## Description
+Package of the Counter Class feature
+```ts
+import { Counter } from "counter-test-package";
+console.log(new Counter(5).inc(2));
+```
+
+### Commands
+To run
+```
+npm start 
+```
+To build
+```
+npm run build 
+```
---
+++ src/Counter.ts
@@ -0,0 +1,33 @@
+export class Counter {
+  value: number
+
+  constructor(n: number) {
+    this.value = n
+  }
+
+  val(): number {
+    return this.value
+  }
+
+  inc(v:number=1): number {
+    if(isNaN(v)){
+    	throw new Error("Value is not number!");
+    }
+    this.value += v
+    return this.val()
+  }
+  
+  dec(v:number=1): number {
+    if(isNaN(v)){ 
+        throw new Error("Value is not number!");
+    }
+    this.value -= v       
+    return this.val()
+  }
+
+
+  toString(): string {
+    return this.val().toString()
+  }
+
+}

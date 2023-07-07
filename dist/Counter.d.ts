02d0b2770bf7b74bef4d8bebad7690e2e77da716
manish <manish.sharma@antechindia.com>
2023-07-07 10:03:00 +0530

    add dist folder

---
+++ dist/Counter.d.ts
@@ -0,0 +1,8 @@
+export declare class Counter {
+    value: number;
+    constructor(n: number);
+    val(): number;
+    inc(v?: number): number;
+    dec(v?: number): number;
+    toString(): string;
+}

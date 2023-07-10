31e278267ef41fef1a1e2c8843e0f3f642bf1fd1
manish <manish.sharma@antechindia.com>
2023-07-10 15:50:56 +0530

    testing for env files

--- src/Counter.ts
+++ src/Counter.ts
@@ -1,33 +1,31 @@
 export class Counter {
-  value: number
+  value: number;
 
   constructor(n: number) {
-    this.value = n
+    this.value = n;
   }
 
   val(): number {
-    return this.value
+    return this.value;
   }
 
-  inc(v:number=1): number {
-    if(isNaN(v)){
-    	throw new Error("Value is not number!");
+  inc(v: number = 1): number {
+    if (isNaN(v)) {
+      throw new Error("Value is not number!");
     }
-    this.value += v
-    return this.val()
-  }
-  
-  dec(v:number=1): number {
-    if(isNaN(v)){ 
-        throw new Error("Value is not number!");
-    }
-    this.value -= v       
-    return this.val()
+    this.value += v;
+    return this.val();
   }
 
+  dec(v: number = 1): number {
+    if (isNaN(v)) {
+      throw new Error("Value is not number!");
+    }
+    this.value -= v;
+    return this.val();
+  }
 
   toString(): string {
-    return this.val().toString()
+    return this.val().toString();
   }
-
 }

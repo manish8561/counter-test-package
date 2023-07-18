cc2d6a468cd449fb76e7ccba87aa26c0997f10ae
manish <manish.sharma@antechindia.com>
2023-07-18 16:05:46 +0530

    changes for cjs, esm, umd, types

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Counter = void 0;
-const Counter_1 = require("./Counter");
-Object.defineProperty(exports, "Counter", { enumerable: true, get: function () { return Counter_1.Counter; } });
